ab9c4a2c0d8677a1d6f34cb6927e1bc7eaafd4de
pverdura <pverdura6@gmail.com>
2023-05-25 17:39:39 +0200

    Disseny dels personatges del joc

---
+++ zeos/graphics.c
@@ -0,0 +1,271 @@
+#include <graphics.h>
+#include <libc.h>
+
+char* sys_color = " ";
+
+
+void setColor(char* color)
+{
+	int color_hex;
+	
+	//We check that if the color we need is already set
+	if(color != sys_color || sys_color == (char*)" ")
+	{
+		if(color == (char*)"red") color_hex = 0x4;
+		else if(color == (char*)"green") color_hex = 0x2;
+		else if(color == (char*)"blue") color_hex = 0x1;
+		else if(color == (char*)"magenta") color_hex = 0x5;
+		else if(color == (char*)"cyan") color_hex = 0x3;
+		else if(color == (char*)"orange") color_hex = 0x6;
+		else if(color == (char*)"white") color_hex = 0x7;
+		else color_hex = 0x0; //"black"
+		
+		set_color(color_hex,color_hex);
+		sys_color = color;
+	}
+}
+
+/*   ###
+ *  #####
+ * # # # #
+ */
+void print_spaceship(int x, int y)
+{
+	
+	setColor("red");
+	//  ***
+	// *****
+	//# * * #
+	gotoxy(x,y);
+	write(1, "#", strlen("#"));
+	gotoxy(x+6,y);
+	write(1, "#", strlen("#"));
+	
+	setColor("white");
+	//  ###
+	// #***#
+	//* # # *
+	gotoxy(x+2,y);
+	write(1, "#", strlen("#"));
+	gotoxy(x+4,y);
+	write(1, "#", strlen("#"));
+	gotoxy(x+1,y-1);
+	write(1, "#####", strlen("#####"));
+	gotoxy(x+2,y-2);
+	write(1, "###", strlen("###"));
+	
+	setColor("cyan");
+	//  ***
+	// *###*
+	//* * * *
+	gotoxy(x+2,y-1);
+	write(1, "###", strlen("###"));
+}
+
+/* # # #
+ *  ###
+ */
+void print_enemy_1(int x, int y)
+{
+	setColor("orange");
+	//* # *
+	// #*#
+	gotoxy(x+2,y-1);
+	write(1, "#", strlen("#"));
+	gotoxy(x+1,y);
+	write(1, "###", strlen("###"));
+	
+	setColor("magenta");
+	//# * #
+	// *#*
+	gotoxy(x,y-1);
+	write(1, "#", strlen("#"));
+	gotoxy(x+4,y-1);
+	write(1, "#", strlen("#"));
+	gotoxy(x+2,y);
+	write(1, "#", strlen("#"));
+}
+
+/*  ###
+ * #####
+ */
+void print_enemy_2(int x, int y)
+{
+	setColor("white");
+	// #*#
+	//#*#*#
+	gotoxy(x+1,y-1);
+	write(1, "###", strlen("###"));
+	gotoxy(x,y);
+	write(1, "#####", strlen("#####"));
+	
+	setColor("green");
+	// *#*
+	//*#*#*
+	gotoxy(x+2,y-1);
+	write(1, "#", strlen("#"));
+	gotoxy(x+1,y);
+	write(1, "#", strlen("#"));
+	gotoxy(x+3,y);
+	write(1, "#", strlen("#"));
+}
+
+//#####
+// # #
+void print_enemy_3(int x, int y)
+{
+	setColor("cyan");
+	//#*#*#
+	// # #
+	gotoxy(x,y-1);
+	write(1, "#####", strlen("#####"));
+	gotoxy(x+1,y);
+	write(1, "#", strlen("#"));
+	gotoxy(x+3,y);
+	write(1, "#", strlen("#"));
+	
+	setColor("blue");
+	//*#*#*
+	// * *
+	gotoxy(x+1,y-1);
+	write(1, "#", strlen("#"));
+	gotoxy(x+3,y-1);
+	write(1, "#", strlen("#"));
+}
+
+
+//   ######
+// ##########
+//#####  #####
+// ##      ##
+//   ##  ##    (12,5)
+void print_boss(int x, int y)
+{
+	setColor("orange");
+	//   ******
+	// ##**##**##
+	//###**  **###
+	// **      **
+	//   **  **
+	gotoxy(x,y-2);
+	write(1, "###", strlen("###"));
+	gotoxy(x+9,y-2);
+	write(1, "###", strlen("###"));
+	gotoxy(x+1,y-3);
+	write(1, "##########", strlen("##########"));
+
+	setColor("green");
+	//   ######
+	// **##**##**
+	//*****  *****
+	// ##      ##
+	//   **  **
+	gotoxy(x+3,y-4);
+	write(1, "######", strlen("######"));
+	gotoxy(x+3,y-3);
+	write(1, "##", strlen("##"));
+	gotoxy(x+7,y-3);
+	write(1, "##", strlen("##"));
+	gotoxy(x+1,y-1);
+	write(1, "##", strlen("##"));
+	gotoxy(x+9,y-1);
+	write(1, "##", strlen("##"));
+
+	setColor("red");
+	//   ******
+	// **********
+	//***##  ##***
+	// **      **
+	//   ##  ##
+	gotoxy(x+3,y-2);
+	write(1, "##", strlen("##"));
+	gotoxy(x+7,y-2);
+	write(1, "##", strlen("##"));
+	gotoxy(x+3,y);
+	write(1, "##", strlen("##"));
+	gotoxy(x+7,y);
+	write(1, "##", strlen("##"));
+}
+
+// ##
+//####
+//######
+//####
+// ##    (6,5)
+void print_rocket_L(int x, int y)
+{
+	setColor("cyan");
+	// **
+	//####
+	//#**###
+	//####
+	// **
+	gotoxy(x,y-3);
+	write(1, "####", strlen("####"));
+	gotoxy(x,y-2);
+	write(1, "######", strlen("######"));
+	gotoxy(x,y-1);
+	write(1, "####", strlen("####"));
+	
+	setColor("magenta");
+	// ##
+	//****
+	//*##***
+	//****
+	// ##
+	gotoxy(x+1,y-4);
+	write(1, "##", strlen("##"));
+	gotoxy(x+1,y-2);
+	write(1, "##", strlen("##"));
+	gotoxy(x+1,y);
+	write(1, "##", strlen("##"));
+}
+
+//   ##
+//  ####
+//######
+//  ####
+//   ##  (6,5)
+void print_rocket_R(int x, int y)
+{
+	setColor("cyan");
+	//   **
+	//  ####
+	//###**#
+	//  ####
+	//   **
+	gotoxy(x+2,y-3);
+	write(1, "####", strlen("####"));
+	gotoxy(x,y-2);
+	write(1, "######", strlen("######"));
+	gotoxy(x+2,y-1);
+	write(1, "####", strlen("####"));
+	
+	setColor("magenta");
+	//   ##
+	//  ****
+	//***##*
+	//  ****
+	//   ##
+	gotoxy(x+3,y-4);
+	write(1, "##", strlen("##"));
+	gotoxy(x+3,y-2);
+	write(1, "##", strlen("##"));
+	gotoxy(x+3,y);
+	write(1, "##", strlen("##"));
+}
+
+void print_heart(int x, int y)
+{
+	set_color(0x4,0x0);
+	gotoxy(x,y);
+	write(1, "<3", strlen("<3"));
+}
+
+void clear_screen()
+{
+	setColor("Black");
+	gotoxy(MAX_X, MAX_Y);
+	char buff[28] = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"; 
+	write(1, buff, strlen(buff));
+}
---
+++ zeos/include/graphics.h
@@ -0,0 +1,31 @@
+/*
+ * graphics.h - Design of game characters
+ */
+#ifndef __GRAPHICS_H__
+#define __GRAPHICS_H__
+
+#define MIN_X 0
+#define MIN_Y 0
+#define MAX_X 79
+#define MAX_Y 24
+
+#define SPSHIP_X 7
+#define SPSHIP_Y 4
+void print_spaceship(int x, int y);
+
+#define ENEMY_X 5
+#define ENEMY_Y 2
+void print_enemy_1(int x, int y);
+void print_enemy_2(int x, int y);
+void print_enemy_3(int x, int y);
+int* enemy_size();
+
+#define BOSS_X 24
+#define BOSS_Y 5
+void print_boss(int x, int y);
+
+void print_heart(int x, int y);
+
+void clear_screen();
+
+#endif /* __GRAPHICS_H__*/
